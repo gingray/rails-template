04189243550f4ffc60ff8e2a204522351a599e4d
gingray <gingray.dev@gmail.com>
2020-06-30 18:08:09 +0200

    improve config for vcr rspec

--- template.rb
+++ template.rb
@@ -46,6 +46,8 @@ insert_into_file 'spec/spec_helper.rb' do
     VCR.configure do |config|
       config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
       config.hook_into :faraday
+      config.configure_rspec_metadata!
+      config.default_cassette_options = { record: :new_episodes }
     end
   RUBY
 end
